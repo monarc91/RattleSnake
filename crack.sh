bbd2f13e17f2c7da71d15d79e745751aa6611379
monarc91 <69116806+monarc91@users.noreply.github.com>
2020-09-14 04:38:40 +0200

    Add upperCase atbegining of the word

--- crack.sh
+++ crack.sh
@@ -5,9 +5,16 @@ doWord(){
 
     while IFS= read -r line
     do
+        length=${#line}
+        majInit=`echo ${line:0:1} |tr '[:lower:]' '[:upper:]'`
+
         echo "$line"
+        echo "$majInit${line:1:length}" # MAJ-word
+
         for k in {1..99}
         do
+            echo "$majInit${line:1:length}$k" # MAJ-word-number
+            echo "$k$majInit${line:1:length}" # number-MAJ-word
             echo "$line$k"
             echo "$k$line"
         done
