603279d006809faa9651ab7387f3de35134a3a27
monarc91 <69116806+monarc91@users.noreply.github.com>
2020-09-17 12:52:20 +0200

    add french departments values

--- viper.sh
+++ viper.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
-while getopts ":c:o:f:d:n:" opt; do
+while getopts ":c:o:f:d:n:t:" opt; do
     case ${opt} in
         c )
           casef="${OPTARG}"
+          ;;
+        t )
+          dept="${OPTARG}"
           ;;
         o )
           order=${OPTARG}
           ;;
         f )
           format=${OPTARG}
+          echo " coming soon"
+          exit 0
           ;;
         d )
           dict=${OPTARG}
           ;;
         n )
           nb=${OPTARG}
+          echo " coming soon"
+          exit 0
           ;;
       
         \? )
           echo    " Invalid Option: -$OPTARG" 1>&2
-          echo    " #=============================================================================#"
-          echo -e " #  Usage: ./viper.sh -d listOFWords.txt <options>                             #"
-          echo -e " #  -d <dictionnary> list of words                                             #"
-          echo -e " #  -c <case> lower:1 Camel:2 UPPER:3 ALL:4                                    #" 
-          echo -e " #  -o <order> normal <or> reverse <or> all                                    #"
-          echo -e " #  -n <speed in k/s> Calcul the number of possibility and time to test them   #"
-          echo -e " #_____________________________________________________________________________#\n"
+          echo    " #==================================================================================#"
+          echo -e " #   Usage: ./viper.sh -d listOFWords.txt <options>                                 #"
+          echo -e " #   -d <dictionnary> list of words                                                 #"
+          echo -e " #   -c <case> lower:1 Camel:2 UPPER:3 ALL:4                                        #" 
+          echo -e " #   -o <order> normal <or> reverse <or> all                                        #"
+          echo -e " #   -n <speed in k/s> Calcul the number of possibility and time to test them       #"
+          echo -e " #__________________________________________________________________________________#\n"
           exit 1
           ;;
     esac
@@ -59,44 +66,283 @@ if [ "$dict" ]
 then
     :
   else
-    echo    " #=============================================================================#"
-    echo -e " #  Usage: ./viper.sh -d listOFWords.txt <options>                             #"
-    echo -e " #  -d <dictionnary> list of words                                             #"
-    echo -e " #  -c <case> lower:1 Camel:2 UPPER:3 ALL:4                                    #" 
-    echo -e " #  -o <order> normal <or> reverse <or> all                                    #"
-    echo -e " #  -n <speed in k/s> Calcul the number of possibility and time to test them   #"
-    echo -e " #_____________________________________________________________________________#\n"
+      
+    echo    " #==================================================================================#"
+    echo -e " #   Usage: ./viper.sh -d listOFWords.txt <options>                                 #"
+    echo -e " #   -d <dictionnary> list of words                                                 #"
+    echo -e " #   -c <case> lower:1 Camel:2 UPPER:3 ALL:4                                        #" 
+    echo -e " #   -o <order> normal <or> revert <or> all                                         #"
+    echo -e " #   -n <speed in k/s> Calcul the number of possibility and time to test them       #"
+    echo -e " #__________________________________________________________________________________#\n"
     exit 1
 fi
+echo "la casse est: ${casef}"
+echo "l'ordre est: ${order}"
+echo "le format est: ${format}"
+echo "la liste est lÃ : ${dict}"
+echo "compte nombre de possibilitÃ©es en: ${nb}"
 
+departements="CPFrance.txt"
+
+if [ -z "$casef" ];then
+    echo "not -c set"
+  else
+    echo "casef vaut ${casef}"
+fi
+declare -a depart
+terr() {
+  j=0
+  dept=$1
+  while IFS= read -r line
+    do
+    if [ ${#dept} -gt 2 ];then
+      depart[0]=$dept
+    else
+      if [[ $line == $dept* ]];then
+          depart[$j]=$line
+          j=$((j + 1))
+      fi
+    fi
+    done < "$departements"
+}
+
+# if [ "$dept" ]
+# then
+#  terr $dept
+#  for p in "${depart[@]}"
+#  do
+#    echo "$p"
+#  done
+# fi
+terr $dept
 
 doWord(){
     file="$1"
     [ ${2} ] && cas=$2 || cas=4
     [ ${3} ] && ord=$3 || ord="all"
-    # echo $ord
+    terre=$4
+
     while IFS= read -r line
     do
         length=${#line}
         majInit=`echo ${line:0:1} |tr '[:lower:]' '[:upper:]'`
         maj=`echo ${line} |tr '[:lower:]' '[:upper:]'`
-        if  [ $cas -eq 1 ] 
+        if  [[ $cas -eq 1 && -z "$terre" ]] 
         then
           echo "$line"
         fi
-        if  [ $cas -eq 2 ] 
+        if [[ $cas -eq 2 && -z "$terre" ]]
         then
           echo "$majInit${line:1:length}" # MAJ-word
         fi
-        if  [ $cas -eq 3 ] 
+        if [[ $cas -eq 3 && -z "$terre" ]]
         then
           echo "${maj}"
         fi
-        if  [ $cas -eq 4 ] 
+        if [[ $cas -eq 4 && -z "$terre" ]]
         then
           echo "$line"
           echo "$majInit${line:1:length}" # MAJ-word
           echo "${maj}"
+        fi
+        
+        if [[ $cas -eq 1 && ! -z "$terre" && "$ord" == "all" ]]
+        then
+          echo "$line"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$line$terre"
+            echo "$terre$line"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$line$p"
+                echo "$p$line"
+              done
+          fi
+        fi
+        if [[ $cas -eq 2 && ! -z "$terre" && "$ord" == "all" ]]  
+        then
+          echo "$majInit${line:1:length}" # MAJ-word
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$majInit${line:1:length}$terre" # MAJ-word
+            echo "$terre$majInit${line:1:length}" # MAJ-word
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$majInit${line:1:length}$p" # MAJ-word
+                echo "$p$majInit${line:1:length}" # MAJ-word
+              done
+          fi
+        fi
+        if [[ $cas -eq 3 && ! -z "$terre" && "$ord" == "all" ]]  
+        then
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "${maj}$terre"
+            echo "$terre${maj}"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "${maj}$p"
+                echo "$p${maj}"
+              done
+          fi
+        fi
+        if [[ $cas -eq 4 && ! -z "$terre" && "$ord" == "all" ]] 
+        then
+          echo "$line"
+          echo "$majInit${line:1:length}" # MAJ-word
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$line$terre"
+            echo "$majInit${line:1:length}$terre" # MAJ-word
+            echo "$terre$line"
+            echo "$terre$majInit${line:1:length}" # MAJ-word
+            echo "${maj}$terre"
+            echo "$terre${maj}"
+          else  
+            for p in "${depart[@]}"
+              do
+                echo "$line$p"
+                echo "$majInit${line:1:length}$p" # MAJ-word
+                echo "$p$line"
+                echo "$p$majInit${line:1:length}" # MAJ-word
+                echo "${maj}$p"
+                echo "$p${maj}"
+              done
+          fi
+        fi
+          if [[ $cas -eq 1 && ! -z "$terre" && "$ord" == "normal" ]]
+        then
+          echo "$line"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$line$terre"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$line$p"
+              done
+          fi
+        fi
+        if [[ $cas -eq 2 && ! -z "$terre" && "$ord" == "normal" ]]  
+        then
+          echo "$majInit${line:1:length}" # MAJ-word
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$majInit${line:1:length}$terre" # MAJ-word
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$majInit${line:1:length}$p" # MAJ-word
+              done
+          fi
+        fi
+        if [[ $cas -eq 3 && ! -z "$terre" && "$ord" == "normal" ]]  
+        then
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "${maj}$terre"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "${maj}$p"
+              done
+          fi
+        fi
+        if [[ $cas -eq 4 && ! -z "$terre" && "$ord" == "normal" ]] 
+        then
+          echo "$line"
+          echo "$majInit${line:1:length}" # MAJ-word
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$line$terre"
+            echo "$majInit${line:1:length}$terre" # MAJ-word
+            echo "$terre$line"
+            echo "$terre$majInit${line:1:length}" # MAJ-word
+            echo "${maj}$terre"
+            echo "$terre${maj}"
+          else  
+            for p in "${depart[@]}"
+              do
+                echo "$line$p"
+                echo "$majInit${line:1:length}$p" # MAJ-word
+                echo "${maj}$p"
+              done
+          fi
+        fi
+          if [[ $cas -eq 1 && ! -z "$terre" && "$ord" == "reverse" ]]
+        then
+          echo "$line"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$terre$line"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$p$line"
+              done
+          fi
+        fi
+        if [[ $cas -eq 2 && ! -z "$terre" && "$ord" == "reverse" ]]  
+        then
+          echo "$majInit${line:1:length}" # MAJ-word
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$terre$majInit${line:1:length}" # MAJ-word
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$p$majInit${line:1:length}" # MAJ-word
+              done
+          fi
+        fi
+        if [[ $cas -eq 3 && ! -z "$terre" && "$ord" == "reverse" ]]  
+        then
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$terre${maj}"
+          else 
+            
+            for p in "${depart[@]}"
+              do
+                echo "$p${maj}"
+              done
+          fi
+        fi
+        if [[ $cas -eq 4 && ! -z "$terre" && "$ord" == "reverse" ]] 
+        then
+          echo "$line"
+          echo "$majInit${line:1:length}" # MAJ-word
+          echo "${maj}"
+          if [ "${#terre}" -gt 2 ]
+          then
+            echo "$terre$line"
+            echo "$terre$majInit${line:1:length}" # MAJ-word
+            echo "$terre${maj}"
+          else  
+            for p in "${depart[@]}"
+              do
+                echo "$p$line"
+                echo "$p$majInit${line:1:length}" # MAJ-word
+                echo "$p${maj}"
+              done
+          fi
         fi
         for k in {1..99}
         do
@@ -165,4 +411,6 @@ doWord(){
     done < "$file" 
 }
 
-doWord "$dict" "$casef" "$order"
+doWord "$dict" "$casef" "$order" "$dept"
+
+# le crime est toujours punit!
