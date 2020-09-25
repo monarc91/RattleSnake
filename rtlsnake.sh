42b35880f9d0649ec0e156f0193fc20d62642c26
monarc91 <69116806+monarc91@users.noreply.github.com>
2020-09-25 18:43:20 +0200

    Update rtlsnake.sh

--- rtlsnake.sh
+++ rtlsnake.sh
@@ -8,12 +8,12 @@
     #                                                                          #
     #                                                                          #
     #==========================================================================#
-    #   viper.sh                                                               #
-    #   VIPER to accommodate dictionnary                                       #
+    #   rtlsnake.sh                                                               #
+    #   RattleSnake to accommodate dictionnary                                       #
     #   Author: Monarc(Marc Segur)                                             #
     #   Contact: pc-mac@mail.com                                               # 
     #   Date: 15 september 2020                                                #
-    #   Version: 0.9.0                                                         #
+    #   Version: 0.9.2                                                         #
     #   Licence:  GPL3                                                         #
     #   Copyright: Monarc(c)2020                                               #
     #__________________________________________________________________________#
