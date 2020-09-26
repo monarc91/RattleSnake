9d3bf1a4bab376c31f3da740ab59e5f15ca9d762
monarc91 <69116806+monarc91@users.noreply.github.com>
2020-09-26 20:37:11 +0200

    add progressBar

--- install.sh
+++ install.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Author : Teddy Skarin 
+# name: ProgressBar 1980's style
+
+# 1. Create ProgressBar function
+# 1.1 Input is currentState($1) and totalState($2)
+ProgressBar (){
+# Process data
+	let _progress=(${1}*100/${2}*100)/100
+	let _done=(${_progress}*4)/10
+	let _left=40-$_done
+# Build progressbar string lengths
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
+
+# 1.2 Build progressbar strings and print the ProgressBar line
+# 1.2.1 Output example:
+# 1.2.1.1 Progress : [########################################] 100%
+printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+
+}
+
+# Variables
+_start=1
+
+# This accounts as the "totalState" variable for the ProgressBar function
+_end=5
+
+
 if echo "monarc" > /usr/bin/test
 then
     rm /usr/bin/test
@@ -7,6 +35,8 @@ then
 else
     result=1
 fi
+
+files=( rtlsnake.sh dateFR.sh dateUS.sh dateMO.sh CPFrance.txt )
 
 if [[ $result == 0 ]]
 then
@@ -23,7 +53,18 @@ then
     echo "------------------------------------------------------------------------------"
     tput setaf 7
     echo -e "install rtlsnake...\n"
-    cp rtlsnake.sh dateFR.sh dateUS.sh dateMO.sh CPFrance.txt /usr/bin
+
+    for fil in "${!files[@]}"
+    do
+        cp "${files[$fil]}" /usr/bin 
+        tput setaf 6
+        ProgressBar "$(($fil+1))" $_end
+        sleep 0.5
+    done
+   
+    
+    tput setaf 3
+    printf '\n\nFinished!\n'
     echo -e "install rtlsnake DONE.\n"
     tput setaf 6
     echo -e "Now you can run rtlsnake by the command rtlsnake.sh"
