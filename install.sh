29a11a6eb03aa42dea1353b99090f57bda7237a5
monarc91 <69116806+monarc91@users.noreply.github.com>
2020-09-27 03:12:46 +0200

    rewrite by monarc and shellcheck.net

--- install.sh
+++ install.sh
@@ -2,27 +2,26 @@
 
 # Author : Teddy Skarin 
 # name: ProgressBar 1980's style
+# rewrite: Monarc 2020
 
 # 1. Create ProgressBar function
 # 1.1 Input is currentState($1) and totalState($2)
-ProgressBar (){
-# Process data
-	let _progress=(${1}*100/${2}*100)/100
-	let _done=(${_progress}*4)/10
-	let _left=40-$_done
-# Build progressbar string lengths
-	_done=$(printf "%${_done}s")
-	_left=$(printf "%${_left}s")
+ProgressBar() 
+    {
+        # Process data
+        	_progress=$(((${1}*100/${2}*100)/100))
+                _done=$(((_progress*4)/10))
+        	_left=$((40-_done))
+        # Build progressbar string lengths
+        	_done=$(printf "%${_done}s")
+        	_left=$(printf "%${_left}s")
+        
+        # 1.2 Build progressbar strings and print the ProgressBar line
+        # 1.2.1 Output example:
+        # 1.2.1.1 Progress : [########################################] 100%
+        printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
+    }
 
-# 1.2 Build progressbar strings and print the ProgressBar line
-# 1.2.1 Output example:
-# 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
-
-}
-
-# Variables
-_start=1
 
 # This accounts as the "totalState" variable for the ProgressBar function
 _end=5
@@ -59,7 +58,7 @@ then
     do
         cp "${files[$fil]}" /usr/bin 
         tput setaf 6
-        ProgressBar "$(($fil+1))" $_end
+        ProgressBar "$((fil+1))" $_end
         tput sc;
         echo -ne " Copy file ${files[$fil]}"
         sleep 0.1
